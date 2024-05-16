4672f98a602508f99001350b7e8e9c49b9a0c043
Linkai Wu <linkai@linkaiwu.com>
2024-05-16 00:41:01 -0400

    Update selected works

--- app/SelectedWorks.tsx
+++ app/SelectedWorks.tsx
@@ -1,66 +1,73 @@
 import ContrastLink from '@/components/ui/contrast-link';
 
+import { Fragment } from 'react';
 import { cn } from '@/utils/cn';
 
 const featured = [
   {
-    title: 'Project 1',
-    description: 'Description',
-    tag: '[TAG]',
+    title: 'Wisconsin Robotics',
+    description: 'Website & outreach',
+    tag: '[UX / Dev / CAD / Electrical]',
     thumbnail: '',
   },
   {
-    title: 'Project 2',
-    description: 'Description',
-    tag: '[TAG]',
+    title: 'STEM to the Sky',
+    description: 'Website & CMS',
+    tag: '[UX / Dev]',
     thumbnail: '',
   },
   {
-    title: 'Project 3',
-    description: 'Description',
-    tag: '[TAG]',
+    title: 'Oracle of Blair',
+    description: 'Website & predictive model',
+    tag: '[UX / Dev / Data]',
     thumbnail: '',
   },
   {
-    title: 'Project 4',
-    description: 'Description',
-    tag: '[TAG]',
+    title: '3D art',
+    description: 'Misc. 3D projects',
+    tag: '[3D]',
     thumbnail: '',
   },
 ];
 
 export default function SelectedWorks({ className, ...rest }: { className?: string, [key: string]: any }) {
   return (
-    <div className={cn("pt-24 pb-36 px-2 relative", className)} {...rest}>
+    <div className={cn("pt-24 pb-24 md:pb-12 px-2 relative overflow-hidden", className)} {...rest}>
       <h2 className="container max-w-3xl text-6xl text-center font-heading">
-        The <span className="text-primary font-cursive">intersection</span> of tech, art, and people is where I like to be.
+        The{' '}
+        <span className="relative text-primary font-cursive">
+          intersection
+          <BgArc width={100} height={60} className="absolute -bottom-[2rem] left-1/2 -translate-x-[calc(50%+2rem)] w-[100rem] -rotate-[10deg] -z-[1]"/>
+          <BgArc width={200} height={150} className="absolute -top-[2.5rem] left-1/2 -translate-x-[calc(50%-9rem)] w-[200rem] rotate-[170deg] -z-[1]"/>
+        </span>
+        {' '}of tech, art, and people is where I like to be.
       </h2>
 
       <div className="container max-w-5xl grid grid-cols-1 md:grid-cols-2 grid-flow-row-dense gap-x-36 lg:gap-x-48 gap-y-24 md:gap-y-12 mt-32">
-        {featured.map((project, i) => <>
+        {featured.map((project, i) => <Fragment key={project.title}>
           {i%2==0 && <div className="hidden md:block h-24"/>} {/* SPACER */}
 
           <div className="row-span-2">
             <div className="aspect-video bg-light/20 rounded-lg"/> {/* THUMBNAIL */}
 
-            <div className={cn("flex gap-8 justify-between mt-1", i%2==1 && "md:flex-row-reverse")}>
-              <div className="md:-ml-10 -mt-6">
-                <h3 className="text-3xl text-primary font-mono font-bold uppercase">
+            <div className={cn("flex gap-8 justify-between mt-1", i%2==0 ? "md:-ml-10" : "md:-mr-10 md:flex-row-reverse")}>
+              <div className={cn("-mt-6", i%2==1 && "md:text-right")}>
+                <h3 className="text-3xl leading-8 text-primary font-mono font-bold uppercase">
                   {project.title}
                 </h3>
-                <p className="text-primary/50 leading-6 font-mono uppercase">
+                <p className="leading-5 text-primary/50 font-mono uppercase">
                   {project.description}
                 </p>
               </div>
 
-              <p className="text-lg text-primary leading-6 font-mono uppercase">
+              <p className={cn("text-lg text-right text-primary leading-6 font-mono uppercase", i%2==1 && "md:text-left")}>
                 {project.tag}
               </p>
             </div>
           </div>
 
           {i%2==1 && <div className="hidden md:block h-24"/>} {/* SPACER */}
-        </>)}
+        </Fragment>)}
         
         <div className="hidden md:block h-48"/> {/* SPACER */}
 
@@ -80,5 +87,13 @@ export default function SelectedWorks({ className, ...rest }: { className?: stri
         </div>
       </div>
     </div>
+  );
+}
+
+function BgArc({ width, height, ...rest }: { width: number, height: number, [key: string]: any }) {
+  return (
+    <svg viewBox={`0 0 ${width} ${height+2}`} xmlns="http://www.w3.org/2000/svg" {...rest}>
+      <path fill="transparent" stroke="#C7956D" strokeOpacity="0.2" strokeWidth={0.2} d={`M0,1 Q${width/2},${height*2+1} ${width},1`}></path>
+    </svg>
   );
 }
