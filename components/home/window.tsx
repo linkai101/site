41af76665aaf048c7865d188354a1dded6302c82
Linkai Wu <linkai@linkaiwu.com>
2024-06-01 15:17:16 -0400

    Add hero in view animation

--- components/home/window.tsx
+++ components/home/window.tsx
@@ -1,12 +1,41 @@
 "use client";
-
-import { motion } from 'framer-motion';
+import { useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
 import { cn } from '@/utils/cn';
 
 export default function Window({ children, className, ...rest }: { children?: React.ReactNode, className?: string, [key: string]: any }) {
+  const ref = useRef<HTMLHeadingElement>(null);
+  const isInView = useInView(ref, { once: true, amount: 0 });
+
   return (
     <motion.div
-      whileTap={{ scale: 1.01, opacity: 0.9 }}
+      ref={ref}
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+      whileTap="interact"
+      variants={{
+        hidden: {
+          opacity: 0,
+          scale: 0.7,
+        },
+        visible: {
+          opacity: 1,
+          scale: 1,
+          transition: {
+            type: 'spring',
+            delay: Math.random() * 0.1,
+            duration: 0.5,
+          }
+        },
+        interact: {
+          scale: 1.01,
+          opacity: 0.9,
+          transition: {
+            type: 'spring',
+            duration: 0.1,
+          }
+        },
+      }}
       drag
       dragMomentum={false}
       dragElastic={0.05}
