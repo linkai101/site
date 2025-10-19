5bf04b922666eaed11ae7cf44b0ba09552bcf9b5
Linkai Wu <linkai@linkaiwu.com>
2025-10-19 12:37:50 -0400

    Update hero 3d elements

--- components/landing/hero/index.tsx
+++ components/landing/hero/index.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion, useScroll, useTransform, useSpring, useMotionValueEvent } from "motion/react";
+import { cn } from "@/lib/utils";
 import { LinkaiOS } from "./linkai-os";
 import { GrainedBackground } from "@/components/ui/grained";
 
@@ -13,60 +14,123 @@ export function HeroSection() {
   const [portionScrolled, setPortionScrolled] = useState(0);
   useMotionValueEvent(smoothYProgress, "change", (latest: number) => setPortionScrolled(latest));
 
+  const [viewportWidth, setViewportWidth] = useState(0);
+  const [viewportHeight, setViewportHeight] = useState(0);
+  useEffect(() => {
+    const updateViewport = () => {
+      setViewportWidth(window.innerWidth);
+      setViewportHeight(window.innerHeight);
+    };
+    
+    updateViewport();
+    window.addEventListener('resize', updateViewport);
+    return () => window.removeEventListener('resize', updateViewport);
+  }, []);
+
   return (
-    <div>
-      <div ref={heroRef} className="h-[300dvh] relative">
+    <div ref={heroRef} className="h-[300dvh] relative">
+      <div className="sticky top-0 h-dvh flex items-center justify-center overflow-hidden">
+        {/* LAPTOP */}
         <motion.div
-          className="sticky top-0 h-dvh"
+          className="perspective-[4000px]"
           style={{
-            rotateY: useTransform(smoothYProgress, [0, 0.8], [0, 30]),
-            skewX: useTransform(smoothYProgress, [0, 0.8], [0, 10]),
-            x: useTransform(smoothYProgress, [0, 0.8], [0, -200]),
             scale: useTransform(smoothYProgress, [0, 0.8], [1, 0.4]),
           }}
         >
-          {/* LAPTOP SCREEN */}
+          {/* LAPTOP LID */}
           <motion.div
-            className="h-full overflow-hidden"
-            animate={{
-              borderTopLeftRadius: portionScrolled > 0 ? 16 : 0,
-              borderTopRightRadius: portionScrolled > 0 ? 16 : 0,
-              opacity: portionScrolled < 0.8 ? 1 : 0.2,
+            className={cn(
+              "pt-6 pb-12 px-6 bg-zinc-800 rounded-t-[3rem] rounded-b-2xl border-x-6 border-t-6 border-zinc-500 origin-bottom backface-hidden transform-3d",
+              "-mt-6 -mb-[calc(3rem-6px)] -mx-6" // margin to offset positioning when expanded
+            )}
+            style={{
+              rotateX: useTransform(smoothYProgress, [0, 0.8, 1], [0, 10, -90]),
             }}
-            transition={{
-              duration: 0.5,
-              ease: "easeInOut",
-            }}
-            // scroll into focus when clicked
-            onPointerDown={() => heroRef.current?.scrollIntoView({ block: "start", behavior: "smooth" })}
           >
-            <LinkaiOS/>
-            <GrainedBackground className="absolute inset-0 pointer-events-none"/>
+            {/* LAPTOP SCREEN */}
+            <motion.div
+              className="relative overflow-hidden"
+              style={{
+                width: useTransform(smoothYProgress, [0, 0.8], [viewportWidth, 1512]),
+                height: useTransform(smoothYProgress, [0, 0.8], [viewportHeight, 982]),
+              }}
+              animate={{
+                borderTopLeftRadius: portionScrolled > 0 ? 16 : 0,
+                borderTopRightRadius: portionScrolled > 0 ? 16 : 0,
+                opacity: portionScrolled < 0.9 ? 1 : 0.2,
+              }}
+              transition={{
+                duration: 0.5,
+                ease: "easeInOut",
+              }}
+              // scroll into focus when clicked
+              onPointerDown={() => heroRef.current?.scrollIntoView({ block: "start", behavior: "smooth" })}
+            >
+              <LinkaiOS allowInteraction={portionScrolled === 0}/>
+              <GrainedBackground className="absolute inset-0 pointer-events-none"/>
+            </motion.div>
+
+            {/* LAPTOP NOTCH */}
+            <motion.div
+              className="absolute top-6 left-1/2 -translate-x-1/2 w-48 h-8 bg-zinc-800 rounded-b-xl z-10"
+              // show when scrolled past 0
+              animate={{ opacity: portionScrolled > 0.05 ? 1 : 0, y: portionScrolled > 0.05 ? 0 : -12 }}
+              transition={{ duration: 0.25, ease: "easeInOut" }}
+            />
           </motion.div>
 
-          {/* LAPTOP FRAME */}
+          {/* LAPTOP BACK */}
           <motion.div
-            className="absolute -top-8 -bottom-12 -left-[calc(2rem+12px)] -right-8 bg-zinc-800 rounded-t-[3rem] border-l-12 border-zinc-500 -z-10"
-            // show when scrolled past 0
-            animate={{ opacity: portionScrolled > 0 ? 1 : 0 }}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-          />
+            className={cn(
+              "absolute inset-x-0 bottom-0 top-2 bg-zinc-500 rounded-[3rem] origin-bottom -translate-y-2 transform-3d -z-10",
+              "-mt-6 -mb-[calc(3rem-6px)] -mx-6" // margin to offset positioning when expanded
+            )}
+            style={{
+              rotateX: useTransform(smoothYProgress, [0, 0.8, 1], [0, 10, -90]),
+            }}
+          >
+            <motion.div // thickness
+              className={cn(
+                "absolute top-0 inset-x-[3rem] h-3 bg-zinc-500 origin-top rotate-x-[90deg] z-10",
+              )}
+            />
 
-          {/* LAPTOP NOTCH */}
-          <motion.div
-            className="absolute top-0 left-1/2 -translate-x-1/2 w-48 h-8 bg-zinc-800 rounded-b-xl"
-            // show when scrolled past 0
-            animate={{ opacity: portionScrolled > 0.05 ? 1 : 0, y: portionScrolled > 0.05 ? 0 : -12 }}
-            transition={{ duration: 0.25, ease: "easeInOut" }}
+            {/* a sad attempt at rounded corners (angled corners) */}
+            <motion.div
+              className={cn(
+                "absolute top-[3rem] left-0 h-3 w-[68px] origin-top-left bg-zinc-500 rotate-x-[90deg] rotate-y-[-45deg] z-10",
+              )}
+            />
+            <motion.div
+              className={cn(
+                "absolute top-[3rem] right-0 h-3 w-[68px] origin-top-right bg-zinc-500 rotate-x-[90deg] rotate-y-[45deg] z-10",
+              )}
+            />
+          </motion.div>
+
+          {/* LAPTOP BASE */}
+          <motion.div // top
+            className={cn(
+              "absolute inset-0 bg-zinc-600 rounded-[3rem] origin-bottom rotate-x-[-90deg] -z-20",
+              "-mt-6 -mb-[calc(3rem-6px)] -mx-6" // margin to offset positioning when expanded
+            )}
+          >
+            {/* keyboard */}
+            <div className="absolute bottom-24 inset-x-16 h-1/2 rounded-2xl bg-zinc-700"/>
+            {/* trackpad */}
+            <div className="absolute top-16 left-1/2 -translate-x-1/2 h-[calc(50%-10rem-3rem)] w-1/3 rounded-2xl bg-zinc-500/50 border-2 border-zinc-500"/>
+          </motion.div>
+          <motion.div // bottom
+            className={cn(
+              "absolute inset-0 bg-zinc-700 rounded-[3rem] origin-bottom rotate-x-[-90deg] translate-y-10 -z-30",
+              "-mt-6 -mb-[calc(3rem-6px)] -mx-6" // margin to offset positioning when expanded
+            )}
           />
-          
-          {/* LAPTOP KEYBOARD */}
-          <motion.div
-            className="absolute top-[calc(100%+6rem)] -left-[calc(2rem+12px)] -right-8 h-32 bg-zinc-600 origin-top-left rotate-x-[60deg] skew-x-[80deg]"
-          />
-          <motion.div className="absolute top-[calc(100%+3rem)] -left-[calc(2rem+12px)] -right-8 h-[3rem] bg-zinc-600 origin-top-left rotate-[1.57deg] scale-x-[1.45] rounded-b-xl"/>
-          <motion.div
-            className="absolute top-[calc(100%+3rem)] -left-[calc(2rem+12px)] -right-8 h-32 bg-zinc-500 origin-top-left rotate-x-[60deg] skew-x-[80deg]" //  rotate-x-[60deg] skew-x-[80deg]
+          <motion.div // thickness
+            className={cn(
+              "absolute bottom-0 inset-x-[3px] h-10 bg-zinc-700 origin-bottom translate-y-10 translate-z-[1030px] -z-30",
+              "-mt-6 -mb-[calc(3rem-6px)] -mx-6" // margin to offset positioning when expanded
+            )}
           />
         </motion.div>
       </div>
--- components/landing/hero/linkai-os.tsx
+++ components/landing/hero/linkai-os.tsx
@@ -1,10 +1,49 @@
+"use client";
 
-export function LinkaiOS() {
+import { motion } from "motion/react";
+import { cn } from "@/lib/utils";
+
+interface LinkaiOSProps {
+  allowInteraction?: boolean;
+}
+
+export function LinkaiOS({ allowInteraction = true }: LinkaiOSProps) {
   return (
-    <div className="h-full relative flex items-center justify-center bg-primary text-on-primary">
+    <div className={cn("h-full relative flex items-center justify-center bg-primary text-on-primary", allowInteraction ? "pointer-events-auto" : "pointer-events-none")}>
       <h1 className="text-11xl text-nowrap text-on-primary/10 font-display uppercase select-none">
         {"I'm Linkai"}
       </h1>
+
+      <motion.div
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[45rem] h-[30rem] bg-background text-foreground rounded-xl shadow-xl"
+        drag
+        dragMomentum={false}
+      >
+        browser
+      </motion.div>
+
+      <motion.div
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[28rem] h-[20rem] bg-background text-foreground rounded-xl shadow-xl"
+        drag
+        dragMomentum={false}
+      >
+      </motion.div>
+
+      <motion.div
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[32rem] h-[25rem] bg-background text-foreground rounded-xl shadow-xl"
+        drag
+        dragMomentum={false}
+      >
+        notes app
+      </motion.div>
+
+      <motion.div
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[32rem] h-[18rem] bg-background text-foreground rounded-xl shadow-xl"
+        drag
+        dragMomentum={false}
+      >
+        finder (links to socials and etc.)
+      </motion.div>
     </div>
   );
 }
